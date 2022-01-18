c976d42141309b6e44ef07097048d02dc80889b9
Nagendrakodavali <97425855+Nagendrakodavali@users.noreply.github.com>
2022-01-18 08:13:59 +0530

    Add files via upload

---
+++ if else.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n;
+	printf("enter the values of n");
+	scanf("%d",&n);
+	{
+		if(n==1)
+		{
+			printf("january\n");
+		}
+		else if(n==2)
+		{
+			printf("febrary");
+		}
+		else if(n==3)
+		{
+			printf("march");
+		}
+		else if(n==4)
+		{
+			printf("april");
+		}
+		else if(n==5)
+		{
+			printf("may");
+		}
+		else if(n==6)
+		{
+			printf("june");
+		}
+		else if(n==7)
+		{
+			printf("july");
+		}
+		else if(n==8)
+		{
+			printf("auguest");
+		}
+		else if(n==9)
+		{
+			printf("september");
+		}
+		else if(n==10)
+		{
+		printf("october");
+		}
+		else if(n==11)
+		{
+		printf("november");
+		}
+		else if(n==12)
+		{
+		printf("december");
+			}
+    }
+}

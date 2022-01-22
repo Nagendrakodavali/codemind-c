1094bdf49e2b901539dcb1cdc4b903f946dc020f
Nagendrakodavali <97425855+Nagendrakodavali@users.noreply.github.com>
2022-01-22 14:39:00 +0530

    Add files via upload

---
+++ arrey reversal using place reverse.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+void main()
+{
+	
+	int n;
+	
+	
+	scanf("%d", &n); 
+	
+	
+	int arr[n], i, j, temp; 
+
+	for(i=0; i<n; i++)
+	{
+		scanf("%d", &arr[i]); 
+	}
+	
+	printf("Array elements before reversing\n");
+	for(i=0; i<n; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	
+
+	for(i=0, j=n-1; i<j; i++, j--)
+	{
+		temp = arr[i]; 
+		arr[i] = arr[j]; 
+		arr[j] = temp;
+	}
+	
+	
+	
+	printf("\n Array after reversal\n");
+	for(i=0; i<n; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

eb226a67ec47f80180554511c5e2a80f077a1dd2
Nagendrakodavali <97425855+Nagendrakodavali@users.noreply.github.com>
2022-02-18 11:50:08 +0530

    Add files via upload

---
+++ str revers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char str[100];
+	scanf("%[^\n]s",str);
+	int len=0,i,j,t;
+	for(i=0;str[i]!='\0';i++)
+	{
+		len=len+1;
+	}
+	i=0;
+	j=len-1;
+	while(i<j)
+	{
+     	t=str[j];
+    	str[j]=str[i];
+    	str[i]=t;
+	i++;
+	j--;	
+	}
+	printf("%s",str);	
+}

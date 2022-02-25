bf407ad13dafe803070320f699ee97b89a1f1bf7
Nagendrakodavali <97425855+Nagendrakodavali@users.noreply.github.com>
2022-02-25 11:39:31 +0530

    Add files via upload

---
+++ representation  of point.c
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+ int main()
+ {
+   int arr[5];
+   int i;
+   for(i=0;i<5;i++)
+   {
+   	scanf("%d",arr+1);
+   	
+	}
+	for(i=0;i<5;i++)
+	{
+		printf("%d\n",*(arr+1));
+	 } 
+   
+ }

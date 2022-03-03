eb6f09a1f1be9529cc4cf5f189e48b3e10705a1d
Nagendrakodavali <97425855+Nagendrakodavali@users.noreply.github.com>
2022-03-03 11:33:14 +0530

    Add files via upload

---
+++ time memory allocation.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+	int n=5;
+	int *arr;
+	arr=(int*)malloc(n*sizeof(int));
+	int i;
+	for(i=0;i<n;i++)
+	{
+		printf("%d %d\n", arr+i,*(arr+i));
+	}
+	printf("before initalizing the array");
+	for(i=0;i<n;i++)
+	{
+		printf("array elements are\n");
+		for(i=0;i<n;i++)
+		{
+			scanf("%d",arr+i);
+		}
+		printf("array elements are\n");
+		for(i=0;i<n;i++)
+		{
+			printf("%d %d\n",arr+i,*(arr+i));
+		}
+		free(arr);
+		for(i=0;i<n;i++)
+		{
+			printf("%d %d\n",arr+i,*(arr+i));
+		}
+	}
+	
+}
